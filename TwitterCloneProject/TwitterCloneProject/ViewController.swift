60edcead4974f92ef3f2b60bd10cdba50118d493
Taeyoung Jin <elisha0103@naver.com>
2023-06-16 17:37:52 +0900

    init
    - Project creates

---
+++ TwitterCloneProject/TwitterCloneProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterCloneProject
+//
+//  Created by 진태영 on 2023/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
