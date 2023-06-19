1ba7502d9c7142741be44b672a6d383e8a8166f7
Taeyoung Jin <elisha0103@naver.com>
2023-06-19 18:26:39 +0900

    Feat: actionButton Function
    - ActionButton 액션 함수 설정

--- TwitterCloneProject/TwitterCloneProject/Controller/MainTabController.swift
+++ TwitterCloneProject/TwitterCloneProject/Controller/MainTabController.swift
@@ -13,8 +13,9 @@ class MainTabController: UITabBarController {
     let actionButton: UIButton = {
         let button: UIButton = UIButton(type: .system)
         button.tintColor = .white
-        button.backgroundColor = .blue
+        button.backgroundColor = .twitterBlue
         button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        button.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
         
         return button
     }()
@@ -27,11 +28,16 @@ class MainTabController: UITabBarController {
         configureUI()
     }
     
+    // MARK: - Selectors
+    @objc func actionButtonTapped() {
+        print("123")
+    }
+    
     // MARK: - Helpers
     func configureUI() {
     
         view.addSubview(actionButton)
-        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
+        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.safeAreaLayoutGuide.rightAnchor, paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
         actionButton.layer.cornerRadius = 56 / 2
     }
     
